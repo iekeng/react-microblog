c5c7ab1db2300eae3a8dbdf849334a7a40a401bd
iekeng <ekeng7.eb@gmail.com>
2023-12-29 05:46:46 +0100

    added bootstraps NavLink for routing withing React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import {BrowserRouter, Routes, Route, Navigate} from react-router-dom;
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import LoginPage from './pages/LoginPage';
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
 
-function App() {
+export default function App() {
     return (
       <>
       <Container fluid className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<FeedPage />}/>
-          <Route path="/explore" element={<LoginPage/>}/>
-          <Route path="/page" element={<ExplorePage />}/>
-          <Route path="*" element={<Navigate to="/" />}/>
-        </Routes>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<FeedPage />}/>
+            <Route path="/explore" element={<LoginPage/>}/>
+            <Route path="/page" element={<ExplorePage />}/>
+            <Route path="*" element={<Navigate to="/" />}/>
+          </Routes>
+        </BrowserRouter>
     </Container>
     </>
   );
 }
-
-export default App;
