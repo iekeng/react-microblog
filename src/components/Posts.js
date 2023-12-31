e33e8c74f12bf8b546733c0052a56b09748c2b6c
iekeng <ekeng7.eb@gmail.com>
2023-12-31 13:27:57 +0100

    more button should navigate through all posts from data base

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,21 +3,35 @@ import { useEffect, useState } from 'react';
 import Post from './Post'
 import { useApi } from '../contexts/ApiProvider';
 
-export default function Posts() {
+export default function Posts({ content }) {
 
     const [posts, setPosts] = useState()
     const api = useApi()
 
+    let url;
+    switch(content) {
+        case 'feed':
+        case 'undefined':
+            url = '/feed';
+            break;
+        case 'explore':
+            url = '/posts'
+            break;
+        default: 
+            url = `/users/${content}/posts`;
+            break;
+    }
+
     useEffect(() => {
         (async () => {
-            let response = await api.get('/feed');
+            const response = await api.get(url);
             if (response.ok) {
                 setPosts(response.body.data)
             } else {
                 setPosts(null)
             }
         })();
-    }, [api])
+    }, [api, url])
 
     return (
         <>
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -6,6 +6,7 @@ import TimeAgo from '../components/TimeAgo'
 import Stack from 'react-bootstrap/Stack';
 import  { useApi } from '../contexts/ApiProvider'
 import { useEffect, useState } from 'react';
+import Posts from '../components/Posts';
 
 export default function UserPage() {
   
@@ -28,19 +29,22 @@ export default function UserPage() {
           <>
               { user === null ? 
                 <p>User not found.</p>
-              :   
+              :  
+              <>
                 <Stack direction="horizontal" gap={4}>
                     { console.log(`${user.first_seen}`) }
                     <Image src="{user.avatar_url}" roundedCircle />
                     <div>
                         <h1>{ user.username }</h1>
                         <p>
-                            Member since: <TimeAgo isoDate={user.first_seen} />
-                            <br />
-                            Last seen: <TimeAgo isoDate={user.last_seen} />
+                          Member since: <TimeAgo isoDate={user.first_seen} />
+                          <br />
+                          Last seen: <TimeAgo isoDate={user.last_seen} />
                         </p>
                     </div>
                 </Stack>
+                <Posts content={user.id}/>
+              </>
               }
           </>}
     </Body>
