766cad2a72de2dbcdd94808941b25f6223ec0326
iekeng <ekeng7.eb@gmail.com>
2023-12-31 15:39:11 +0100

    chapter 5 complete

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,51 +1,71 @@
-import Spinner from 'react-bootstrap/Spinner'
+import Spinner from 'react-bootstrap/Spinner';
 import { useEffect, useState } from 'react';
-import Post from './Post'
+import Post from './Post';
 import { useApi } from '../contexts/ApiProvider';
+import More from './More';
 
 export default function Posts({ content }) {
 
-    const [posts, setPosts] = useState()
-    const api = useApi()
+  const [posts, setPosts] = useState();
+  const [pagination, setPagination] = useState();
+  const api = useApi();   
 
-    let url;
-    switch(content) {
-        case 'feed':
-        case 'undefined':
-            url = '/feed';
-            break;
-        case 'explore':
-            url = '/posts'
-            break;
-        default: 
-            url = `/users/${content}/posts`;
-            break;
+  let url;
+  switch(content) {
+      case 'feed':
+      case 'undefined':
+          url = '/feed';
+          break;
+      case 'explore':
+          url = '/posts'
+          break;
+      default: 
+          url = `/users/${content}/posts`;
+          break;
+  }
+
+  useEffect(() => {
+    (async () => {
+        const response = await api.get(url);
+        if (response.ok) {
+            setPosts(response.body.data)
+            setPagination(response.body.pagination)
+        } else {
+            setPosts(null)
+        }
+    })();
+  }, [api, url])
+
+  const loadNextPage = async () => {
+    const response = await api.get(url, {
+      after: posts[posts.length - 1].timestamp
+    });
+    if (response.ok) {
+      setPosts([...posts, ...response.body.data]);
+      setPagination(response.body.pagination);
     }
+  };
 
-    useEffect(() => {
-        (async () => {
-            const response = await api.get(url);
-            if (response.ok) {
-                setPosts(response.body.data)
-            } else {
-                setPosts(null)
-            }
-        })();
-    }, [api, url])
-
-    return (
-        <>
-            { posts === undefined ?
-             < Spinner border="animation" /> 
-             :
+  return (
+    <>
+      { posts === undefined ?
+        < Spinner animation="border" /> 
+        :
+          <>
+              {posts === null ? 
+                  <p>Could not retrieve blog posts.</p> 
+              :
                 <>
-                    {posts.length === null ? 
-                        <p>Could not load blog posts.</p> 
-                    :
-                        posts.map(post => <Post key={ post.id } post={ post }/>)
-                    }
+                  {posts.length === 0 ? 
+                    <p>There are no blog posts.</p>
+                  :
+                    posts.map(post => <Post key={ post.id } post={ post }/>)
+                  }
+                  <More pagination={pagination} loadNextPage={loadNextPage}/>
                 </>
-            }
-        </>
-    );
+              }
+          </>
+      }
+    </>
+  );
 }
